0895c0e5f7c3a9387b5c6a0eaee7ba349e34b6de
LydianJay <lloydjayedradan@gmail.com>
2025-10-26 14:33:52 +0800

    return back the button for the navigation in mobile view

--- resources/views/components/dashboard/basedashboard.blade.php
+++ resources/views/components/dashboard/basedashboard.blade.php
@@ -102,6 +102,23 @@
         <!-- Navbar -->
         <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
             navbar-scroll="true">
+            {{-- Nav --}}
+
+            <div class="container-fluid py-1 px-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                        <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                            <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                                <div class="sidenav-toggler-inner">
+                                    <i class="sidenav-toggler-line"></i>
+                                    <i class="sidenav-toggler-line"></i>
+                                    <i class="sidenav-toggler-line"></i>
+                                </div>
+                            </a>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
             
             <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
 
@@ -110,10 +127,6 @@
         </nav>
         <!-- End Navbar -->
         <div class="container-fluid py-4">
-
-
-
-
             {{ $slot }}
         </div>
 
